fe7513a065c39e3238713e1f4ba05cf8ff205a72
Darian <darian.jorgevich@est.fi.uncoma.edu.ar>
2021-09-16 17:05:37 -0300

    Actualizacion traductor

--- src/TPO1/patrones/main.java
+++ src/TPO1/patrones/main.java
@@ -14,59 +14,6 @@ import java.util.Scanner;
  */
 public class main {
 
-    public static void menu() {
-        System.out.println("-----------------------------------------------------------------------------");
-        System.out.println("        Bienvenidos a nuestro traductor seleccione la opcion que desee");
-        System.out.println("-----------------------------------------------------------------------------");
-        System.out.println("1) Traducir al español alguna palabra / oracion");
-        System.out.println("2) Correr un ejemplo de prueba por defecto");
-        System.out.println("0) Salir");
-    }
-
-    public static void menu1Texto() {
-        System.out.println("-----------------------------------------------------------------------------");
-        System.out.println("Seleccione: ");
-        System.out.println("1) Colocar una palabra / oracion");
-        System.out.println("0) Salir");
-    }
-
-    public static void traducir(String[] toTranslate) throws IOException {
-        TranslatorApp translatorApp = new TranslatorApp();
-
-        for (String text : toTranslate) {
-            translatorApp.translateToSp(text);
-        }
-    }
-
-    public static void menu1() throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int op = 1, i = 0;
-        while (op != 0) {
-            menu1Texto();
-            op = sc.nextInt();
-            switch (op) {
-                case 1:
-                    traducirPalabra();
-                    break;
-                case 0:
-                    break;
-                default:
-                    System.out.println("Opcion no valida");
-                    break;
-
-            }
-        }
-    }
-
-    public static void traducirPalabra() throws IOException {
-        Scanner sc = new Scanner(System.in);
-        String palabra;
-        System.out.println("Escriba la palabra / oracion");
-        palabra = sc.nextLine();
-        String[] p = {palabra};
-        traducir(p);
-    }
-
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int op = 1;
@@ -79,7 +26,7 @@ public class main {
                     break;
                 case 2:
                     String[] t = {"卵", "I am english", "Ich bin Deutscher", "je suis Français", "buon pomeriggio ragazzi", "Olá mundo, sou portugues", "Norsk skrevet tekst"};
-                    traducir(t);
+                    translate(t);
                     break;
                 case 0:
                     String name = sc.nextLine();
@@ -90,4 +37,57 @@ public class main {
             }
         }
     }
+    
+    public static void menu() {
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("        Bienvenidos a nuestro traductor seleccione la opcion que desee");
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("1) Traducir al español alguna palabra / oracion");
+        System.out.println("2) Correr un ejemplo de prueba por defecto");
+        System.out.println("0) Salir");
+    }
+
+    public static void menu1() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int op = 1, i = 0;
+        while (op != 0) {
+            menu1Text();
+            op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    translateWord();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+
+            }
+        }
+    }
+    
+    public static void menu1Text() {
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Seleccione: ");
+        System.out.println("1) Colocar una palabra / oracion");
+        System.out.println("0) Salir");
+    }
+
+    public static void translate(String[] toTranslate) throws IOException {
+        TranslatorApp translatorApp = new TranslatorApp();
+
+        for (String text : toTranslate) {
+            translatorApp.translateToSp(text);
+        }
+    }
+
+    public static void translateWord() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String word;
+        System.out.println("Escriba la palabra / oracion");
+        word = sc.nextLine();
+        String[] p = {word};
+        translate(p);
+    }   
 }
