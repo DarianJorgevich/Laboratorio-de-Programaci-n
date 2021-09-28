cbd4d1c86a9075bd0b228212e1bb1f5f3c7a2af2
Lucas San Segundo <lucas.sansegundo@est.fi.uncoma.edu.ar>
2021-09-27 21:44:42 -0300

    cambio chico

--- src/patronbridge/preguntas/EjemploPreguntas.java
+++ src/patronbridge/preguntas/EjemploPreguntas.java
@@ -1,28 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package patronbridge.preguntas;
 
 
-import java.util.Scanner;
 
-/**
- *
- * @author Lucas
- */
+
 public class EjemploPreguntas {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         
-        Primario a= new Primario ("Lucas", new Geografia());
-        Secundario b= new Secundario ("Darian", new Historia());
-        Trivia triv = new Trivia(a);
-        triv.iniciarTrivia();
+        Primario lucas= new Primario ("Lucas", new Geografia());
+        Secundario darian= new Secundario ("Darian", new Historia());
+        new Trivia(darian).iniciarTrivia();
+       
     }
     
 }
