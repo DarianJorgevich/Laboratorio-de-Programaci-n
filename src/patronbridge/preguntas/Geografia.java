eda98facca5489396fa8427a1f0169b5ae1f6327
Lucas San Segundo <lucas.sansegundo@est.fi.uncoma.edu.ar>
2021-09-27 21:46:21 -0300

    sacando comentarios al pedo

--- src/patronbridge/preguntas/Geografia.java
+++ src/patronbridge/preguntas/Geografia.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package patronbridge.preguntas;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Lucas
- */
 public class Geografia implements Pregunta {
 
     private final List<String> preguntasFaciles = new ArrayList<>();
--- src/patronbridge/preguntas/Pregunta.java
+++ src/patronbridge/preguntas/Pregunta.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package patronbridge.preguntas;
 
 
-/**
- *
- * @author Lucas
- */
+
 public interface Pregunta {
     
     public void mostrarPreguntaFacil(int num);
