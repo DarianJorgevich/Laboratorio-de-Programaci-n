3782f03874ad50213f5fa7354711bb9069fc6f5b
Darian <darian.jorgevich@est.fi.uncoma.edu.ar>
2021-11-30 10:52:08 -0300

    Agrego GET con parametros en productos.js

--- src/rutas/productos.js
+++ src/rutas/productos.js
@@ -7,10 +7,31 @@ const fs = require('fs');
 const json_productos = fs.readFileSync('src/productos.json', 'utf-8'); //leemos el Json
 let productos = JSON.parse(json_productos);
 
-router.get('/', (req, res) => { //envia el json
-    res.json(productos);
-    console.log('Se envio: ', productos);
-})
+/*router.get(['/','/:id'], (req, res) => { //envia el json
+    var idProd = req.params.id;
+    console.log('id parametro: ',idProd);
+    res.json(productos[idProd]);
+    //console.log('Se envio: ', productos);
+});*/
+//Metodo GET por id. Si se ingresa mal id, se envian todos los elementos
+router.get(['/', '/:id', '/:cantidad' & '/:desde'], (req, res) => { //envia el json
+    console.log('Parametros query: ', JSON.stringify(req.query));
+    const idProd = req.query.id;
+    const cantProd = req.query.cantidad;
+    const desdeProd = req.query.desde;
+    if (idProd && !cantProd && !desdeProd) {    //Fue la forma que se me ocurrio de verificar que solo manden id
+        res.json(productos[idProd]);
+    } else {
+        if (cantProd && desdeProd && !idProd) {
+            console.log('Se deberia enviar un rango desde:',desdeProd,' - La cantidad de:',cantProd)
+            res.json((productos).slice(desdeProd,JSON.parse(desdeProd)+JSON.parse(cantProd)));
+        } else {
+            res.json(productos);
+            console.log('Se envio: ', productos);
+        }
+    }
+});
+
 
 router.post('/', async (req, res) => {
     const { titulo, desc, ubicacion, alt } = req.body;
