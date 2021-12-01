6e0fb15a42e3a16aa9e2c1aef58e93c1f7dced4f
Darian <darian.jorgevich@est.fi.uncoma.edu.ar>
2021-12-01 13:19:39 -0300

    Modificacion del paginador, como tambien la carga inicial de la pestaña productos

--- src/public/cartas.js
+++ src/public/cartas.js
@@ -1,8 +1,6 @@
 var divIndex = document.getElementById("cardDiv");
-var divProdu = document.getElementById("contenidoProd");
 
 mostrarCartas(4,0,divIndex);
-mostrarCartas(5,0,divProdu); 
 
 function mostrarCartas(cantidad, desde, unDiv) { //funcion que va a mostrar una cantidad de cards desde algun punto del arreglo
   fetch(`http://localhost:3000/api/productos?cantidad=${cantidad}&desde=${desde}`) //le pedimos a la API la cantidad de productos que necesitamos
--- src/public/paginador.js
+++ src/public/paginador.js
@@ -3,30 +3,57 @@ const bot2 = document.getElementById("contBoton2");
 const botPrev = document.getElementById("contPrev");
 const botNext = document.getElementById("contNext");
 
-function boton1(cantidad, desde) {
+var pagActual = 1;
+const itemsPorPag = 5;
+var ultimoItemActual = 0;
+
+boton1();
+
+/*function boton1(cantidad, desde) {
     bot1.className = "page-item active";
     bot2.className = "page-item";
     botPrev.className = "page-item disabled";
     botNext.className = "page-item";
     mostrarCartas(cantidad, desde, document.getElementById("contenidoProd"));
+}*/
+function boton1() {
+    bot1.className = "page-item active";
+    bot2.className = "page-item";
+    botPrev.className = "page-item disabled";
+    botNext.className = "page-item";
+    ultimoItemActual = 0;
+    mostrarCartas(itemsPorPag, ultimoItemActual, document.getElementById("contenidoProd"));
+    ultimoItemActual = ultimoItemActual + itemsPorPag;
+    console.log('ultimo item actual despues de boton 1: ', ultimoItemActual);
 }
-function boton2(cantidad, desde) {
+
+/*function boton2(cantidad, desde) {
     bot1.className = "page-item";
     bot2.className = "page-item active";
     botPrev.className = "page-item";
     botNext.className = "page-item disabled";
     mostrarCartas(cantidad, desde, document.getElementById("contenidoProd"));
+}*/
+function boton2() {
+    bot1.className = "page-item";
+    bot2.className = "page-item active";
+    botPrev.className = "page-item";
+    botNext.className = "page-item disabled";
+    mostrarCartas(itemsPorPag, ultimoItemActual, document.getElementById("contenidoProd"));
+    ultimoItemActual = ultimoItemActual + itemsPorPag;
+    console.log('ultimo item actual despues de boton 2: ', ultimoItemActual);
 }
+
 function prev() {
     if (bot2.className == "page-item active") {
-        boton1(5, 0);
+        boton1();
     }
     botPrev.className = "page-item disabled";
     botNext.className = "page-item";
 }
 function next() {
     if (bot1.className == "page-item active") {
-        boton2(5, 5);
+        boton2();
     }
     botPrev.className = "page-item";
     botNext.className = "page-item disabled";
