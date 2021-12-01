88bb0b428fadf522d5c41cc8ed6f9a3215f371b4
Lucas San Segundo <lucas.sansegundo@est.fi.uncoma.edu.ar>
2021-12-01 16:51:12 -0300

    Validaciones

--- src/rutas/productos.js
+++ src/rutas/productos.js
@@ -4,6 +4,8 @@ const router = Router();
 const _ = require('underscore');
 const fs = require('fs');
 
+
+
 const json_productos = fs.readFileSync('src/productos.json', 'utf-8'); //leemos el Json
 let productos = JSON.parse(json_productos);
 
@@ -83,6 +85,45 @@ router.put('/:id', async (req, res) => {
 })
 
 
+//------------------------------- Validaciones de datos -------------------------------------
+
+function validarProducto(titulo, desc, ubicacion, alt) {
+    //se valida los campos que tiene el producto
+    //tiene que estar correctos todos los campos: titulo, descripcion del producto, ubicacion de la foto y alt.
+    var esCorrecto = true;
+    if (!titulo || !desc || !ubicacion || !alt) {
+        esCorrecto = false;
+    }
+    return esCorrecto;
+}
+
+ function validarID(unID, productos) {
+    //metodo para validar un ID en el JSON productos
+    //devuelve la posicion del arreglo donde esta el ID
+    //si el ID es -1 quiere decir que no lo encontro
+    var pos = -1;
+    _.each(productos, (producto, i) => {
+        //por cada producto va verificando si su id es el mismo que el parametro
+        if (producto.id == unID) {
+            pos = i;
+        };
+    });
+    return pos;
+}
+
+//esta funcion escribe los cambios en el arreglo de productos a memoria
+//retorna un valor booleando que nos dice si fue escrito o no con exito
+function escritura(productos) {
+    var correcto = true;
+    const json_productos = JSON.stringify(productos);
+    fs.writeFile('src/productos.json', json_productos, (err) => {
+        if (err) {
+            correcto = false;
+            throw err;
+        }
+    });
+    return correcto;
+}
 
 
 module.exports = router;
